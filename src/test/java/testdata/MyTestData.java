ca965daa2d0d7f58be76712b61316e9b94be05ab
Oberdiah <richardjmullender@gmail.com>
2025-05-12 00:01:29 +0100

    Updated the tests

--- src/test/java/testdata/MyTestData.java
+++ src/test/java/testdata/MyTestData.java
@@ -4,8 +4,8 @@ import com.github.oberdiah.deepcomplexity.RequiredScore;
 
 public class MyTestData {
 	@RequiredScore(1.0)
-	public static short test1(short incoming) {
-		if (incoming < 3) {
+	public static short test1(short x) {
+		if (x < 3) {
 			return 2;
 		} else {
 			return 1;
@@ -13,40 +13,40 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short test2(short incoming) {
-		if (incoming < 3) {
-			return incoming;
+	public static short test2(short x) {
+		if (x < 3) {
+			return x;
 		} else {
 			return 0;
 		}
 	}
 	
 	@RequiredScore(1.0)
-	public static short test3(short incoming) {
-		if (incoming < 5 && incoming < 3) {
-			return incoming;
+	public static short test3(short x) {
+		if (x < 5 && x < 3) {
+			return x;
 		} else {
 			return 0;
 		}
 	}
 	
 	@RequiredScore(0.75) // We've not implemented even/odd/modulo detection yet.
-	public static short test4(short incoming1) {
-		int bar = incoming1;
+	public static short test4(short x) {
+		int bar = x;
 		
-		if (incoming1 > 2) {
-			incoming1 = (short) (-incoming1);
-			bar += incoming1;
+		if (x > 2) {
+			x = (short) (-x);
+			bar += x;
 		}
 		
-		return (short) (incoming1 + bar);
+		return (short) (x + bar);
 	}
 	
 	@RequiredScore(1.0)
-	public static short test5(short incoming) {
-		if (incoming < 5) {
-			if (incoming < 3) {
-				return incoming;
+	public static short test5(short x) {
+		if (x < 5) {
+			if (x < 3) {
+				return x;
 			} else {
 				return 0;
 			}
@@ -56,113 +56,113 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short negateTest(short incoming) {
-		return (short) (-incoming);
+	public static short negateTest(short x) {
+		return (short) (-x);
 	}
 	
 	@RequiredScore(1.0)
-	public static short negateTest2(short incoming) {
-		return (short) (-incoming + incoming);
+	public static short negateTest2(short x) {
+		return (short) (-x + x);
 	}
 	
 	@RequiredScore(1.0)
-	public static short plusTest(short incoming) {
-		return (short) (+incoming);
+	public static short plusTest(short x) {
+		return (short) (+x);
 	}
 	
 	@RequiredScore(1.0)
-	public static short incrementTest(short incoming) {
-		int a = incoming;
+	public static short incrementTest(short x) {
+		int a = x;
 		int b = a++;
 		int c = a;
 		return (short) (b - c);
 	}
 	
 	@RequiredScore(1.0)
-	public static short incrementTest2(short incoming) {
-		int a = incoming;
+	public static short incrementTest2(short x) {
+		int a = x;
 		int b = ++a;
 		int c = a;
 		return (short) (b - c);
 	}
 	
 	@RequiredScore(1.0)
-	public static short decrementTest(short incoming) {
-		int a = incoming;
+	public static short decrementTest(short x) {
+		int a = x;
 		int b = a--;
 		int c = a;
 		return (short) (b - c);
 	}
 	
 	@RequiredScore(1.0)
-	public static short decrementTest2(short incoming) {
-		int a = incoming;
+	public static short decrementTest2(short x) {
+		int a = x;
 		int b = --a;
 		int c = a;
 		return (short) (b - c);
 	}
 	
 	@RequiredScore(1.0)
-	public static short zeroTest(short incoming) {
-		return (short) (incoming - incoming);
+	public static short zeroTest(short x) {
+		return (short) (x - x);
 	}
 	
 	@RequiredScore(1.0)
-	public static short zeroTest2(short incoming) {
-		return (short) (incoming * 2 - incoming * 2);
+	public static short zeroTest2(short x) {
+		return (short) (x * 2 - x * 2);
 	}
 	
 	@RequiredScore(1.0)
-	public static short zeroTest3(short incoming) {
-		return (short) ((incoming + 1) * 2 - (incoming + 1) * 2);
+	public static short zeroTest3(short x) {
+		return (short) ((x + 1) * 2 - (x + 1) * 2);
 	}
 	
 	@RequiredScore(1.0)
-	public static short zeroTest4(short incoming) {
-		int a = ((incoming * 2) - 1) * 2;
-		int b = ((incoming * 2) - 1) * 2;
+	public static short zeroTest4(short x) {
+		int a = ((x * 2) - 1) * 2;
+		int b = ((x * 2) - 1) * 2;
 		return (short) (a - b);
 	}
 	
 	@RequiredScore(0.5) // We've not implemented even/odd/modulo detection yet.
-	public static short simpleAdd(short incoming1) {
-		return (short) (incoming1 + incoming1);
+	public static short simpleAdd(short x) {
+		return (short) (x + x);
 	}
 	
 	@RequiredScore(1.0)
-	public static short shortTest1(short incoming) {
-		return incoming;
+	public static short shortTest1(short x) {
+		return x;
 	}
 	
 	@RequiredScore(1.0)
-	public static short shortTest2(short incoming) {
-		return (short) (((int) incoming) + 1);
+	public static short shortTest2(short x) {
+		return (short) (((int) x) + 1);
 	}
 	
 	@RequiredScore(1.0)
-	public static short shortTest3(short incoming) {
-		return (short) (incoming + 1);
+	public static short shortTest3(short x) {
+		return (short) (x + 1);
 	}
 	
 	@RequiredScore(1.0)
-	public static short multiplyTest1(short incoming) {
+	public static short multiplyTest1(short x) {
 		int a = 0;
 		int b = 0;
-		if (incoming > 0 && incoming < 10) {
-			a = incoming;
-			b = incoming;
+		if (x > 0 && x < 10) {
+			a = x;
+			b = x;
 		}
 		
 		return (short) (a * 2 - b * 2);
 	}
 	
 	@RequiredScore(1.0)
-	public static short barTest1(short incoming1) {
+	public static short barTest1(short x) {
 		int bar = 0;
 		
-		if (incoming1 > 2) {
-			incoming1 = (short) (-incoming1);
-			bar += incoming1;
+		if (x > 2) {
+			x = (short) (-x);
+			bar += x;
 		} else {
 			bar += 5;
 		}
@@ -171,12 +171,12 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(0.5)
-	public static short barTest2(short incoming1) {
+	public static short barTest2(short x) {
 		int bar = 0;
 		
-		if (incoming1 > 2) {
-			incoming1 = (short) (-incoming1);
-			bar += incoming1;
+		if (x > 2) {
+			x = (short) (-x);
+			bar += x;
 		} else {
 			bar += 5;
 		}
@@ -184,12 +184,12 @@ public class MyTestData {
 		return (short) (bar * 2);
 	}
 	
-	public static short barTest3(short incoming1) {
+	public static short barTest3(short x) {
 		int bar = 0;
 		
-		if (incoming1 > 2) {
-			incoming1 = (short) (-incoming1);
-			bar += incoming1;
+		if (x > 2) {
+			x = (short) (-x);
+			bar += x;
 		} else {
 			bar += 5;
 		}
@@ -198,48 +198,48 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(0.3334)
-	public static short largeTest1(short incoming1) {
+	public static short largeTest1(short x) {
 		int foo = 0;
 		int bar = 0;
-		int incoming2 = incoming1;
-		if (incoming1 < -10) {
-			incoming2 = 0;
+		int a = x;
+		if (x < -10) {
+			a = 0;
 		}
 		
-		incoming2 += 1;
+		a += 1;
 		
-		if (incoming1 > 2) {
-			incoming2 = (short) (incoming1 * incoming2);
-			incoming1 = (short) (-incoming1);
-			bar += incoming1;
+		if (x > 2) {
+			a = (short) (x * a);
+			x = (short) (-x);
+			bar += x;
 			foo += 3;
-		} else if (incoming2 < 3) {
+		} else if (a < 3) {
 			foo += 4;
 		} else {
 			bar += 5;
 		}
 		
-		return (short) (incoming2 * foo + bar * 2);
+		return (short) (a * foo + bar * 2);
 	}
 	
 	@RequiredScore(0.5)
-	public static short multiplicationTest1(short incoming1) {
-		return (short) (incoming1 * (incoming1 + 1));
+	public static short multiplicationTest1(short x) {
+		return (short) (x * (x + 1));
 	}
 	
 	@RequiredScore(1.0)
-	public static short combinedIfs1(short incoming1) {
-		int a = incoming1;
-		if (incoming1 < 0) {
+	public static short combinedIfs1(short x) {
+		int a = x;
+		if (x < 0) {
 			a = 0;
-		} else if (incoming1 > 10) {
+		} else if (x > 10) {
 			a = 10;
 		}
 		
-		int b = incoming1;
-		if (incoming1 < 10) {
+		int b = x;
+		if (x < 10) {
 			b = 0;
-		} else if (incoming1 > 20) {
+		} else if (x > 20) {
 			b = 10;
 		}
 		
@@ -266,18 +266,18 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short combinedIfs3(short incoming1) {
-		int a = incoming1;
-		if (incoming1 < 0) {
+	public static short combinedIfs3(short x) {
+		int a = x;
+		if (x < 0) {
 			a = 0;
-		} else if (incoming1 >= 10) {
+		} else if (x >= 10) {
 			a = 0;
 		}
 		
-		int b = incoming1;
-		if (incoming1 < 10) {
+		int b = x;
+		if (x < 10) {
 			b = 0;
-		} else if (incoming1 > 20) {
+		} else if (x > 20) {
 			b = 0;
 		}
 		
@@ -303,42 +303,42 @@ public class MyTestData {
 		return (short) (a * b);
 	}
 	
-	public static short chainedConstraintTest1(short incoming) {
-		int x = 0;
-		int y = incoming + 5;
+	public static short chainedConstraintTest1(short x) {
+		int a = 0;
+		int b = x + 5;
 		
-		if (incoming > y) {
-			if (y > 5) {
-				x = incoming;
+		if (x > b) {
+			if (b > 5) {
+				a = x;
 			}
 		}
 		
-		return (short) (x);
+		return (short) (a);
 	}
 	
-	public static short chainedConstraintTest2(short incoming) {
-		int x = 0;
-		int y = incoming * 5;
+	public static short chainedConstraintTest2(short x) {
+		int a = 0;
+		int b = x * 5;
 		
-		if (incoming > y && y > 5) {
-			x = incoming;
+		if (x > b && b > 5) {
+			a = x;
 		}
 		
-		return (short) (x);
+		return (short) (a);
 	}
 	
-	public static short intelliJTest1(short incoming) {
+	public static short intelliJTest1(short x) {
 		int a = 0;
 		int b = 0;
-		if (incoming > 0) {
+		if (x > 0) {
 			a = 1;
 		}
-		if (incoming * 5 > 0) {
+		if (x * 5 > 0) {
 			b = 1;
 		}
 		
 		short v = 0;
-		if (a + b > 1 && incoming < 0) {
+		if (a + b > 1 && x < 0) {
 			v = 1;
 		}
 		
@@ -346,78 +346,78 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(0.122)
-	public static short constraintTest1(short incoming) {
+	public static short constraintTest1(short x) {
 		int a = 0;
 		int b = 0;
-		if (incoming > 0 && incoming < 10) {
-			a = incoming;
+		if (x > 0 && x < 10) {
+			a = x;
 		}
-		if (incoming > 0 && incoming < 30) {
-			b = incoming;
+		if (x > 0 && x < 30) {
+			b = x;
 		}
 		
 		return (short) (a * b);
 	}
 	
 	@RequiredScore(1.0)
-	public static short constraintTest2(short incoming) {
+	public static short constraintTest2(short x) {
 		int a = 0;
-		if (incoming > 10) {
-			a = incoming;
+		if (x > 10) {
+			a = x;
 		}
-		if (incoming < 20) {
-			a = incoming + 5;
+		if (x < 20) {
+			a = x + 5;
 		}
 		
 		int b = 0;
-		if (incoming < 10) {
+		if (x < 10) {
 			b = a;
 		}
 		return (short) b;
 	}
 	
 	@RequiredScore(1.0)
-	public static short constraintTest3(short incoming) {
+	public static short constraintTest3(short x) {
 		int a = 0;
-		if (incoming > 10) {
-			a = incoming;
+		if (x >= 10) {
+			a = x;
 		}
-		if (incoming < 20) {
-			a = incoming + 5;
+		if (x < 20) {
+			a = x + 5;
 		}
 		
 		int b = 0;
-		if (incoming < 10) {
+		if (x < 10) {
 			b = a;
 		}
 		return (short) b;
 	}
 	
 	@RequiredScore(1.0)
-	public static short constraintTest4(short incoming) {
+	public static short constraintTest4(short x) {
 		int a = 0;
-		if (incoming > 7 || incoming < 2) {
-			a = incoming;
+		if (x > 7 || x < 2) {
+			a = x;
 		}
 		
 		int b = 0;
-		if (incoming < 10) {
+		if (x < 10) {
 			b = a;
 		}
 		return (short) b;
 	}
 	
 	@RequiredScore(1.0)
-	public static short returnTest1(short incoming) {
-		if (incoming > 0) {
-			return incoming;
+	public static short returnTest1(short x) {
+		if (x > 0) {
+			return x;
 		}
 		
-		if (incoming > -10) {
+		if (x > -10) {
 			return -5;
 		}
 		
-		if (incoming > -20) {
+		if (x > -20) {
 			return -10;
 		}
 		
@@ -425,13 +425,13 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short returnTest2(short i) {
+	public static short returnTest2(short x) {
 		int a = -5;
 		
-		if (i > 0) {
-			if (i < 20) {
+		if (x > 0) {
+			if (x < 20) {
 				a = -10;
-				return i;
+				return x;
 			}
 			a += -15;
 		}
@@ -440,15 +440,15 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short returnTest3(short incoming) {
-		if (incoming > 0) {
-			if (incoming < 20) {
+	public static short returnTest3(short x) {
+		if (x > 0) {
+			if (x < 20) {
 				return 5;
 			}
 		}
 		
 		int a = 0;
-		if (incoming > 10) {
+		if (x > 10) {
 			a = 1;
 		}
 		
@@ -456,13 +456,13 @@ public class MyTestData {
 	}
 	
 	@RequiredScore(1.0)
-	public static short returnTest4(short incoming) {
-		if (incoming > 0) {
-			if (incoming < 10) {
+	public static short returnTest4(short x) {
+		if (x > 0) {
+			if (x < 10) {
 				return 5;
 			}
 		} else {
-			if (incoming > -10) {
+			if (x > -10) {
 				return 10;
 			} else {
 				return 20;
@@ -470,7 +470,7 @@ public class MyTestData {
 		}
 		
 		int a = 0;
-		if (incoming > 15) {
+		if (x > 15) {
 			a = 1;
 		}
 		
